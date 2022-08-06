881c27cecfe7e49d30f453f287050a3d1ffc205e
meherpriyanka <meherpriyanka62@gmail.com>
2022-08-06 07:21:13 -0700

    sms-01: Entity class created

---
+++ src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,55 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	@Id
+	private Integer studentId;
+	
+	private String studentName;
+	
+	private String studentEmail;
+	
+	private int studentAge;
+
+	public Integer getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getStudentEmail() {
+		return studentEmail;
+	}
+
+	public void setStudentEmail(String studentEmail) {
+		this.studentEmail = studentEmail;
+	}
+
+	public int getStudentAge() {
+		return studentAge;
+	}
+
+	public void setStudentAge(int studentAge) {
+		this.studentAge = studentAge;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentEmail=" + studentEmail
+				+ ", studentAge=" + studentAge + "]";
+	}
+}
+
